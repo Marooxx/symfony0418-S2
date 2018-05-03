0657a7d2414738c4091a6e097509f5836e3cafc4
Jerome <ambroise.jerome.formation@gmail.fr>
2018-05-03 17:02:45 +0200

    Surcharge FOS : register, layout, formulaire des roles

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -18,4 +18,36 @@ class User extends FOSParentUser
      */
     protected $id;
 
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\IdentityUser", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $identity;
+
+    public function getIdentity(): ?IdentityUser
+    {
+        return $this->identity;
+    }
+
+    public function setIdentity(IdentityUser $identity): self
+    {
+        $this->identity = $identity;
+
+        return $this;
+    }
+
+    /**
+     * Place un rôle unique à l'utilisateur (supprimer tous les anciens rôles)
+     * @param string $userRole
+     */
+    public function setRole(string $userRole)
+    {
+        // Vider les rôles
+        foreach ($this->getRoles() as $role) {
+            $this->removeRole($role);
+        }
+        // Ajout le rôle unique passé en paramètre
+        $this->addRole($userRole);
+    }
+
 }
